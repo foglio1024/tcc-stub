03452191fafc7707739e58c86c72a3f3194ce65c
Foglio1024 <foglio1024@gmail.com>
2018-07-26 18:35:32 +0200

    fix module.json, new caali's directives in index.kjs

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var tcc;
 var commandChannel = -2 >>>0;
 var useLfg = true;
 const Command = require('command');
-module.exports = function TccStub(dispatch) {
+module.exports = function (tcc_stub) {
 
     var net = require('net');
     var HOST = '127.0.0.50';
     var PORT = 9550;
-    const command = Command(dispatch);
+    const command = Command(tcc_stub);
 
     if(srv != false)
     {
@@ -142,7 +142,7 @@ module.exports = function TccStub(dispatch) {
         var itemUid = Number.parseInt(message.substring(message.indexOf('&uid=')+5, message.indexOf('&name=')));
         var senderName = message.substring(message.indexOf('&name=')+6);
 
-        dispatch.send('C_SHOW_ITEM_TOOLTIP_EX',2,{
+        tcc_stub.send('C_SHOW_ITEM_TOOLTIP_EX',2,{
             type: 17,
             id: itemUid,
             unk1: 0,
@@ -158,7 +158,7 @@ module.exports = function TccStub(dispatch) {
     function serveNonDbInfoRequest(message){
         var itemId = Number.parseInt(message.substring(message.indexOf('&id=') + 4));
 
-        dispatch.send('C_REQUEST_NONDB_ITEM_INFO', 2,{
+        tcc_stub.send('C_REQUEST_NONDB_ITEM_INFO', 2,{
             item: itemId,
             unk1: 0,
             button: 0
@@ -169,7 +169,7 @@ module.exports = function TccStub(dispatch) {
         var srvId = Number.parseInt(message.substring(message.indexOf('&srvId=')+7, message.indexOf('&name=')));
         var targetName = message.substring(message.indexOf('&name=') + 6);
 
-        dispatch.send('C_ASK_INTERACTIVE', 2,{
+        tcc_stub.send('C_ASK_INTERACTIVE', 2,{
             unk: 1,
             serverId: srvId,
             name: targetName
@@ -179,7 +179,7 @@ module.exports = function TccStub(dispatch) {
     function serveInspect(message){
         var targetName = message.substring(message.indexOf('&name=') + 6);
 
-        dispatch.send('C_REQUEST_USER_PAPERDOLL_INFO', 1, {
+        tcc_stub.send('C_REQUEST_USER_PAPERDOLL_INFO', 1, {
             name: targetName
         });
     }
@@ -189,7 +189,7 @@ module.exports = function TccStub(dispatch) {
         var raidByte = Number.parseInt(message.substring(message.indexOf('&raid=') + 6));
         var dataArray = new Buffer.alloc(1, raidByte);
 
-        dispatch.send('C_REQUEST_CONTRACT', 1,{
+        tcc_stub.send('C_REQUEST_CONTRACT', 1,{
             type: 4,
             unk2: 0,
             unk3: 0,
@@ -201,7 +201,7 @@ module.exports = function TccStub(dispatch) {
     //inv_guild&name=name
     function serveGuildInvite(message){
         var targetName = message.substring(message.indexOf('&name=') + 6);
-        dispatch.send('C_INVITE_USER_TO_GUILD', 1 ,{
+        tcc_stub.send('C_INVITE_USER_TO_GUILD', 1 ,{
             unk1: 0,
             unk2: 0,
             name: targetName,
@@ -212,7 +212,7 @@ module.exports = function TccStub(dispatch) {
         var targetName = message.substring(message.indexOf('&name=') + 6, message.indexOf('&msg='));
         var msg = message.substring(message.indexOf('&msg=')+ 5);
 
-        dispatch.send('C_ADD_FRIEND', 1,{
+        tcc_stub.send('C_ADD_FRIEND', 1,{
             name: targetName,
             message: msg
         })
@@ -221,7 +221,7 @@ module.exports = function TccStub(dispatch) {
     function serveBlockUser(message){
         var targetName = message.substring(message.indexOf('&name=') + 6);
 
-        dispatch.send('C_BLOCK_USER', 1, {
+        tcc_stub.send('C_BLOCK_USER', 1, {
             name: targetName
         });
     }
@@ -229,7 +229,7 @@ module.exports = function TccStub(dispatch) {
     function serveUnblockUser(message){
         var targetName = message.substring(message.indexOf('&name=') + 6);
 
-        dispatch.send('C_REMOVE_BLOCKED_USER', 1, {
+        tcc_stub.send('C_REMOVE_BLOCKED_USER', 1, {
             name: targetName
         });
     }
@@ -237,7 +237,7 @@ module.exports = function TccStub(dispatch) {
     function serveUnfriendUser(message){
         var targetName = message.substring(message.indexOf('&name=') + 6);
 
-        dispatch.send('C_DELETE_FRIEND', 1, {
+        tcc_stub.send('C_DELETE_FRIEND', 1, {
             name: targetName
         });
     }
@@ -249,7 +249,7 @@ module.exports = function TccStub(dispatch) {
         data.writeUInt32LE(playerId, 0);
         data.writeUInt32LE(listingId, 4);
 
-        dispatch.send('C_REQUEST_CONTRACT', 1,{
+        tcc_stub.send('C_REQUEST_CONTRACT', 1,{
             type:35,
             unk2:0,
             unk3:0,
@@ -263,7 +263,7 @@ module.exports = function TccStub(dispatch) {
         var pId = message.substring(message.indexOf('&player=') + 8, message.indexOf('&listing'));
         var listingId = message.substring(message.indexOf('&listing=') + 9);
 
-        dispatch.send('C_TRADE_BROKER_REJECT_SUGGEST', 1,{
+        tcc_stub.send('C_TRADE_BROKER_REJECT_SUGGEST', 1,{
             playerId: pId,
             listing: listingId
         })
@@ -272,14 +272,14 @@ module.exports = function TccStub(dispatch) {
     function servePartyInfoRequest(message){
         var lfgId = Number.parseInt(message.substring(message.indexOf('&id=') + 4));
 
-        dispatch.send('C_REQUEST_PARTY_INFO', 2, {
+        tcc_stub.send('C_REQUEST_PARTY_INFO', 2, {
             playerId: lfgId
         })
     }
 	//lfg_apply_req&id=lfgId
     function serveApplyToLfgRequest(message){
         var lfgId = Number.parseInt(message.substring(message.indexOf('&id=') + 4));
-        dispatch.send('C_APPLY_PARTY', 1, {
+        tcc_stub.send('C_APPLY_PARTY', 1, {
             playerId: lfgId
         })
     }
@@ -287,7 +287,7 @@ module.exports = function TccStub(dispatch) {
     function serveApplyDecline(message){
         var playerId = message.substring(message.indexOf('&player=') + 8);
 
-        dispatch.send('C_PARTY_APPLICATION_DENIED', 1,{
+        tcc_stub.send('C_PARTY_APPLICATION_DENIED', 1,{
             pid: playerId
         })
     }
@@ -295,7 +295,7 @@ module.exports = function TccStub(dispatch) {
     function serveChatLink(message){
 
         var msg = message.substring(message.indexOf('&') + 1);
-        dispatch.send('S_CHAT',2,{
+        tcc_stub.send('S_CHAT',2,{
             channel: 18,
             authorID: 0,
             unk1: 0,
@@ -308,7 +308,7 @@ module.exports = function TccStub(dispatch) {
 	//loot_settings
 	function serveLootSettings(message){
 
-        dispatch.send('S_CHAT',2,{
+        tcc_stub.send('S_CHAT',2,{
             channel: 18,
             authorID: 0,
             unk1: 0,
@@ -324,7 +324,7 @@ module.exports = function TccStub(dispatch) {
         var pId = Number.parseInt(message.substring(message.indexOf('&pId=') + 5, message.indexOf('&power=')));
         var p = Number.parseInt(message.substring(message.indexOf('&power=') + 7));
 
-        dispatch.send('C_CHANGE_PARTY_MEMBER_AUTHORITY', 1,{
+        tcc_stub.send('C_CHANGE_PARTY_MEMBER_AUTHORITY', 1,{
             serverId: sId,
             playerId: pId,
             canInvite: p
@@ -335,7 +335,7 @@ module.exports = function TccStub(dispatch) {
         var sId = Number.parseInt(message.substring(message.indexOf('&sId=') + 5, message.indexOf('&pId=')));
         var pId = Number.parseInt(message.substring(message.indexOf('&pId=') + 5));
 
-        dispatch.send('C_CHANGE_PARTY_MANAGER',2,{
+        tcc_stub.send('C_CHANGE_PARTY_MANAGER',2,{
             serverId: sId,
             playerId: pId
         })
@@ -345,7 +345,7 @@ module.exports = function TccStub(dispatch) {
         var sId = Number.parseInt(message.substring(message.indexOf('&sId=') + 5, message.indexOf('&pId=')));
         var pId = Number.parseInt(message.substring(message.indexOf('&pId=') + 5));
 
-        dispatch.send('C_BAN_PARTY_MEMBER',1,{
+        tcc_stub.send('C_BAN_PARTY_MEMBER',1,{
             serverId: sId,
             playerId: pId
         })
@@ -354,7 +354,7 @@ module.exports = function TccStub(dispatch) {
 	function serveLfgPageRequest(message){
 		var pagePar = "&page=";
 		var p = Number.parseInt(message.substring(message.indexOf(pagePar) + pagePar.length));
-		dispatch.send('C_REQUEST_PARTY_MATCH_INFO_PAGE',1,{
+		tcc_stub.send('C_REQUEST_PARTY_MATCH_INFO_PAGE',1,{
 			page: p,
 			unk1: 3,
 			unk2: 0
@@ -367,14 +367,14 @@ module.exports = function TccStub(dispatch) {
 		var msg = message.substring(message.indexOf(msgPar) + msgPar.length, message.indexOf(isRaidPar));
 		var raid = message.substring(message.indexOf(isRaidPar) + isRaidPar.length) === "true";
 
-		dispatch.send('C_REGISTER_PARTY_INFO', 1, {
+		tcc_stub.send('C_REGISTER_PARTY_INFO', 1, {
 			isRaid: raid,
 			message: msg
 		});
 	}
 	//lfg_remove
 	function serveRemoveLfg(message){
-		dispatch.send('C_UNREGISTER_PARTY_INFO', 1, {
+		tcc_stub.send('C_UNREGISTER_PARTY_INFO', 1, {
 			unk1: 20,
 			minLevel: 1,
 			maxLevel: 65,
@@ -386,34 +386,34 @@ module.exports = function TccStub(dispatch) {
 	}
 	//lfg_publicize
 	function servePublicizeLfg(message){
-		dispatch.send('C_REQUEST_PARTY_MATCH_LINK', 1, { });
+		tcc_stub.send('C_REQUEST_PARTY_MATCH_LINK', 1, { });
 	}
 	//reset_instance
 	function serveResetRequest(message){
-		dispatch.send('C_RESET_ALL_DUNGEON', 1, { });
+		tcc_stub.send('C_RESET_ALL_DUNGEON', 1, { });
 	}
 	//disband_group
 	function serveDisbandRequest(message){
-		dispatch.send('C_DISMISS_PARTY', 1, { });
+		tcc_stub.send('C_DISMISS_PARTY', 1, { });
 	}
 	//leave_party
 	function serveLeavePartyRequest(message){
-		dispatch.send('C_LEAVE_PARTY', 1, { });
+		tcc_stub.send('C_LEAVE_PARTY', 1, { });
 	}
 	//request_candidates
 	function serveRequestCandidates(message){
-		dispatch.send('C_REQUEST_CANDIDATE_LIST', 1, { });
+		tcc_stub.send('C_REQUEST_CANDIDATE_LIST', 1, { });
 	}
 	//lfg_request_list&minlvl=minlvl&maxlvl=maxlvl
 	function serveRequestLfgList(message){
-		dispatch.send("C_PARTY_MATCH_WINDOW_CLOSED", 1, {});
+		tcc_stub.send("C_PARTY_MATCH_WINDOW_CLOSED", 1, {});
 		var minlvlPar = "&minlvl=";
 		var maxlvlPar = "&maxlvl=";
 		var min = Number.parseInt(message.substring(message.indexOf(minlvlPar) + minlvlPar.length) , message.indexOf(maxlvlPar));
 		var max = Number.parseInt(message.substring(message.indexOf(maxlvlPar) + maxlvlPar.length));
 		if(min > max) min = max;
 		if(min < 1) min = 1;
-		dispatch.send("C_REQUEST_PARTY_MATCH_INFO", 1, {
+		tcc_stub.send("C_REQUEST_PARTY_MATCH_INFO", 1, {
 			unk1: 0,
 			minlvl: min,
 			maxlvl: max,
@@ -424,19 +424,19 @@ module.exports = function TccStub(dispatch) {
 
 	}
 	//block player tooltips
-    dispatch.hook('S_ANSWER_INTERACTIVE', 2 ,(event) => {
+    tcc_stub.hook('S_ANSWER_INTERACTIVE', 2 ,(event) => {
         return false;
     });
 	//block ingame lfg details window if using tcc one
-	dispatch.hook("S_PARTY_MEMBER_INFO", 3, (event) => {
+	tcc_stub.hook("S_PARTY_MEMBER_INFO", 3, (event) => {
 		return !useLfg;
 	});
 	//block ingame lfg window if using tcc one
-	dispatch.hook("S_SHOW_PARTY_MATCH_INFO", 1, (event) => {
+	tcc_stub.hook("S_SHOW_PARTY_MATCH_INFO", 1, (event) => {
 		return !useLfg;
 	});
 	//block tcc messages from gpk file
-    dispatch.hook('S_CHAT', 2 ,(event) => {
+    tcc_stub.hook('S_CHAT', 2 ,(event) => {
         if(event.authorName == 'tccChatLink')
         {
             return false;
@@ -444,7 +444,7 @@ module.exports = function TccStub(dispatch) {
         else{return true;}
     });
 	//hook Command messages to display them in tcc {order: 999, filter:{fake:true}}
-    dispatch.hook('S_PRIVATE_CHAT', 1, {order: 999, filter:{fake:true}}, event =>{
+    tcc_stub.hook('S_PRIVATE_CHAT', 1, {order: 999, filter:{fake:true}}, event =>{
 		/* Commands:
 		 * - :tcc-chatMode:int  0/1
 		 * - :tcc-uiMode:int    0/1
